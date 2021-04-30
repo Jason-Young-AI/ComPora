42331948130374320e5cf3054dd3a6fe853dc204
Jason-Young-AI <AI.Jason.Young@outlook.com>
2021-04-30 11:20:38 +0800

    Fix a tiny bug

--- compora/standardize.py
+++ compora/standardize.py
@@ -1,4 +1,16 @@
-import re
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2021-04-29 16:32
+#
+# This source code is licensed under the WTFPL license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import regex as re
 
 
 def standardize(sentence, language):
@@ -11,13 +23,13 @@ def standardize_punctuation(sentence, language):
     sentence = re.sub(r'\)', ') ', sentence)
     sentence = re.sub(r'\( ', '(', sentence)
     sentence = re.sub(r' \)', ')', sentence)
-    sentence = re.sub(r' ([%:;,.!?])', '\g<1>', sentence)
+    sentence = re.sub(r' ([\.\?%!,:;])', '\g<1>', sentence)
 
     # Quotation Punctuation
     re.sub(r'\'\'', '"', sentence)
     if language in {'en', }:
         # English "quotation," followed by comma, style
-        sentence = re.sub(r'"([,.]+)', '\g<1>"', sentence)
+        sentence = re.sub(r'"([\.,]+)', '\g<1>"', sentence)
     if language in {'cs', 'cz'}:
         # Czech is confused
         pass
