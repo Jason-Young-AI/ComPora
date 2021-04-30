fcd3fdc33485266ddb9860821b6a8deec21c0d34
Jason-Young-AI <AI.Jason.Young@outlook.com>
2021-04-30 11:20:21 +0800

    Update License

--- compora/nb_prefix.py
+++ compora/nb_prefix.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2021-04-29 16:32
+#
+# This source code is licensed under the WTFPL license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 nb_prefix = {
     'ca': {'1': set(), '2': set()},
     'cs': {'1': set(), '2': set()},
