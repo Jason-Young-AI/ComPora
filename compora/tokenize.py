c8aac11326bc0392612e6885befd19ab4f7fb6a2
Jason-Young-AI <AI.Jason.Young@outlook.com>
2021-04-30 11:22:45 +0800

    Replace re with regex, \w with \p{IsAlnum} and \d with \p{N}

--- compora/tokenize.py
+++ compora/tokenize.py
@@ -1,4 +1,18 @@
-import re
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2021-04-29 23:07
+#
+# This source code is licensed under the WTFPL license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import regex as re
+
+
 from yoolkit.text import unicode_category
 
 from compora.nb_prefix import nb_prefix
@@ -6,7 +20,25 @@ from compora.nb_prefix import nb_prefix
 
 def tokenize(sentence, language):
     # seperate out all special characters that are not in 'L' and 'Nd' categories
-    sentence = re.sub(r'([^\w\d\s\'\-,.])', ' \g<1> ', sentence)
+    if language in {'fi', 'sv', }:
+        # In Finnish and Swedish, the colon can be used inside words as an apostrophe-like character:
+        # USA:n, 20:een, EU:ssa, USA:s, S:t
+        sentence = re.sub(r'([^\p{IsAlnum}\s\'\-\.`,:])', ' \g<1> ', sentence)
+        # If a colon is not immediately followed by lower-case characters, separate it out anyway
+        sentence = re.sub(r'(:)(?=$|[^\p{Ll}])', ' \g<1> ', sentence)
+    elif language in {'tdt', }:
+        # In Tetun, the apostrophe can be used inside words as an apostrophe-like character:
+        sentence = re.sub(r'([^\p{IsAlnum}\s\'\-\.`,])', ' \g<1> ', sentence)
+        # If an apostrophe is not immediately followed by lower-case characters, separate it out anyway
+        sentence = re.sub(r'(\')(?=$|[^\p{Ll}])', ' \g<1> ', sentence)
+    elif language in {'ca', }:
+        # In Catalan, the middle dot can be used inside words:
+        # il·lusio
+        sentence = re.sub(r'([^\p{IsAlnum}\s\'\-\.`,·])', ' \g<1> ', sentence)
+        # If a middot is not immediately followed by lower-case characters, separate it out anyway
+        sentence = re.sub(r'(·)(?=$|[^\p{Ll}])', ' \g<1> ', sentence)
+    else:
+        sentence = re.sub(r'([^\p{IsAlnum}\s\'\-\.`,])', ' \g<1> ', sentence)
 
     # Do not split multi-dots
     sentence = re.sub(r'\.(\.+)', ' MULTIDOT\g<1>', sentence)
@@ -15,28 +47,37 @@ def tokenize(sentence, language):
         sentence = re.sub(r'MULTIDOT\.', 'MULTIMULTIDOT', sentence)
 
     # if pattern is not the form of digit,digit, seperate ','
-    sentence = re.sub(r'([^\d]),', '\g<1> , ', sentence)
-    sentence = re.sub(r',([^\d])', ' , \g<1>', sentence)
+    sentence = re.sub(r'([^\p{N}]),', '\g<1> , ', sentence)
+    sentence = re.sub(r',([^\p{N}])', ' , \g<1>', sentence)
 
-    sentence = re.sub(r'([\d]),$', '\g<1> ,', sentence)
+    sentence = re.sub(r'([\p{N}]),$', '\g<1> ,', sentence)
 
     # split contractions
     if language in {'en', }:
-        # right side split
-        sentence = re.sub(r'([^\w])\'([^\w])', '\g<1> \' \g<2>', sentence)
-        sentence = re.sub(r'([\w])\'([^\w])', '\g<1> \' \g<2>', sentence)
+        # Contractions' right side split
+        sentence = re.sub(r'([^\p{IsAlpha}])\'([^\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
+        sentence = re.sub(r'([\p{IsAlpha}])\'([^\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
 
-        sentence = re.sub(r'([^\w\d])\'([\w])', '\g<1> \' \g<2>', sentence)
+        sentence = re.sub(r'([^\p{IsAlpha}\p{N}])\'([\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
 
-        sentence = re.sub(r'([\w])\'([\w])', '\g<1> \'\g<2>', sentence)
-        sentence = re.sub(r'([\d])\'([s])', '\g<1> \'\g<2>', sentence)
-    elif language in {'fr', 'it', 'ga'}:
-        # left side split
-        sentence = re.sub(r'([^\w])\'([^\w])', '\g<1> \' \g<2>', sentence)
-        sentence = re.sub(r'([\w])\'([^\w])', '\g<1> \' \g<2>', sentence)
+        sentence = re.sub(r'([\p{IsAlpha}])\'([\p{IsAlpha}])', '\g<1> \'\g<2>', sentence)
 
-        sentence = re.sub(r'([^\w])\'([\w])', '\g<1> \' \g<2>', sentence)
-        sentence = re.sub(r'([\w])\'([\w])', '\g<1>\' \g<2>', sentence)
+        # Special case for "1990's"
+        sentence = re.sub(r'([\p{N}])\'([s])', '\g<1> \'\g<2>', sentence)
+
+    elif language in {'fr', 'it', 'ga', 'ca', }:
+        # Contractions' left side split
+        sentence = re.sub(r'([^\p{IsAlpha}])\'([^\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
+        sentence = re.sub(r'([\p{IsAlpha}])\'([^\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
+
+        sentence = re.sub(r'([^\p{IsAlpha}])\'([\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
+        sentence = re.sub(r'([\p{IsAlpha}])\'([\p{IsAlpha}])', '\g<1>\' \g<2>', sentence)
+
+    elif language in {'so', 'tdt', }:
+        # Don't split glottals
+        sentence = re.sub(r'([^\p{IsAlpha}])\'([^\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
+        sentence = re.sub(r'([\p{IsAlpha}])\'([^\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
+        sentence = re.sub(r'([^\p{IsAlpha}])\'([\p{IsAlpha}])', '\g<1> \' \g<2>', sentence)
 
     else:
         sentence = re.sub(r'\'', '\' ', sentence)
@@ -46,7 +87,7 @@ def tokenize(sentence, language):
         word = words[index]
         if re.search(r'^(\S+)\.$', word) is not None:
             prefix = re.search(r'^(\S+)\.$', word).group(1)
-            if re.search(r'\.', prefix) and re.search(r'\w', prefix):
+            if re.search(r'\.', prefix) and re.search(r'\p{IsAlpha}', prefix):
                 pass
             elif prefix in nb_prefix[language]['1']:
                 pass
@@ -57,8 +98,10 @@ def tokenize(sentence, language):
             else:
                 word = prefix + ' .'
         words[index] = word
+
     sentence = ' '.join(words)
     sentence = ' '.join(sentence.split())
+
     sentence = re.sub(r'\.\' ?$', ' . \' ', sentence)
 
     while re.search(r'MULTIMULTIDOT', sentence):
@@ -69,5 +112,5 @@ def tokenize(sentence, language):
 
 
 def split_aggressive_hyphen(sentence):
-    sentence = re.sub(r'([\w\d])-(?=[\w\d])', '\g<1> @-@ ', sentence) 
+    sentence = re.sub(r'([\p{IsAlnum}])-(?=[\p{IsAlnum}])', '\g<1> @-@ ', sentence) 
     return sentence
